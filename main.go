b18adfe71d1f53dc85698dd660978d0f8e5e1df4
Nancy Park <nancypark06@gmail.com>
2026-08-17 17:49:52 -0700

    gofmt cleanup

--- main.go
+++ main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-"context"
-"fmt"
-"log"
-"time"
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-"github.com/nancyparkk/gcal-popup/internal/calendar"
-googlecal "google.golang.org/api/calendar/v3"
+	"github.com/nancyparkk/gcal-popup/internal/calendar"
+	googlecal "google.golang.org/api/calendar/v3"
 )
 
 func main() {
-ctx := context.Background()
+	ctx := context.Background()
 
-srv, err := calendar.NewService(ctx)
-if err != nil {
-log.Fatalf("Unable to create calendar service: %v", err)
-}
+	srv, err := calendar.NewService(ctx)
+	if err != nil {
+		log.Fatalf("Unable to create calendar service: %v", err)
+	}
 
-start := time.Now().Add(1 * time.Hour)
-end := start.Add(30 * time.Minute)
+	start := time.Now().Add(1 * time.Hour)
+	end := start.Add(30 * time.Minute)
 
-event := &googlecal.Event{
-Summary: "gcal-popup test event",
-Start: &googlecal.EventDateTime{
-DateTime: start.Format(time.RFC3339),
-},
-End: &googlecal.EventDateTime{
-DateTime: end.Format(time.RFC3339),
-},
-}
+	event := &googlecal.Event{
+		Summary: "gcal-popup test event",
+		Start: &googlecal.EventDateTime{
+			DateTime: start.Format(time.RFC3339),
+		},
+		End: &googlecal.EventDateTime{
+			DateTime: end.Format(time.RFC3339),
+		},
+	}
 
-created, err := srv.Events.Insert("primary", event).Do()
-if err != nil {
-log.Fatalf("Unable to create event: %v", err)
-}
+	created, err := srv.Events.Insert("primary", event).Do()
+	if err != nil {
+		log.Fatalf("Unable to create event: %v", err)
+	}
 
-fmt.Printf("Event created: %s\n", created.HtmlLink)
+	fmt.Printf("Event created: %s\n", created.HtmlLink)
 }
