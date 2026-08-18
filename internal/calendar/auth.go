f1ac7579a3185f0970ed8d8583cf69a3de991285
Nancy Park <nancypark06@gmail.com>
2026-08-17 17:47:08 -0700

    fixed formatting

--- internal/calendar/auth.go
+++ internal/calendar/auth.go
@@ -1,82 +1,82 @@
 package calendar
 
 import (
-"context"
-"encoding/json"
-"fmt"
-"log"
-"net/http"
-"os"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-"golang.org/x/oauth2"
-"golang.org/x/oauth2/google"
-"google.golang.org/api/calendar/v3"
-"google.golang.org/api/option"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
 )
 
 func getClient(config *oauth2.Config) *http.Client {
-tokFile := "token.json"
-tok, err := tokenFromFile(tokFile)
-if err != nil {
-tok = getTokenFromWeb(config)
-saveToken(tokFile, tok)
-}
-return config.Client(context.Background(), tok)
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
 }
 
 func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-fmt.Printf("Go to the following link in your browser, then paste the authorization code:\n%v\n", authURL)
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser, then paste the authorization code:\n%v\n", authURL)
 
-var authCode string
-if _, err := fmt.Scan(&authCode); err != nil {
-log.Fatalf("Unable to read authorization code: %v", err)
-}
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
 
-tok, err := config.Exchange(context.TODO(), authCode)
-if err != nil {
-log.Fatalf("Unable to retrieve token from web: %v", err)
-}
-return tok
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
 }
 
 func tokenFromFile(file string) (*oauth2.Token, error) {
-f, err := os.Open(file)
-if err != nil {
-return nil, err
-}
-defer f.Close()
-tok := &oauth2.Token{}
-err = json.NewDecoder(f).Decode(tok)
-return tok, err
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
 }
 
 func saveToken(path string, token *oauth2.Token) {
-fmt.Printf("Saving credential file to: %s\n", path)
-f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-if err != nil {
-log.Fatalf("Unable to cache oauth token: %v", err)
-}
-defer f.Close()
-json.NewEncoder(f).Encode(token)
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
 
 func NewService(ctx context.Context) (*calendar.Service, error) {
-b, err := os.ReadFile("credentials.json")
-if err != nil {
-return nil, fmt.Errorf("unable to read credentials.json: %w", err)
-}
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		return nil, fmt.Errorf("unable to read credentials.json: %w", err)
+	}
 
-config, err := google.ConfigFromJSON(b, calendar.CalendarEventsScope)
-if err != nil {
-return nil, fmt.Errorf("unable to parse credentials.json: %w", err)
-}
+	config, err := google.ConfigFromJSON(b, calendar.CalendarEventsScope)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse credentials.json: %w", err)
+	}
 
-client := getClient(config)
+	client := getClient(config)
 
-srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
-if err != nil {
-return nil, fmt.Errorf("unable to create calendar service: %w", err)
-}
-return srv, nil
+	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create calendar service: %w", err)
+	}
+	return srv, nil
 }
